5252b2d2b466d33d1631ec36ed66d5fa1749d644
Basemism <95645899+Basemism@users.noreply.github.com>
2024-03-07 22:10:35 +0000

    x

--- script.js
+++ script.js
@@ -18,20 +18,61 @@ document.querySelectorAll('.reveal').forEach(text => {
     type: 'words'
   })
 
-  // Animation
+    // Animation
   const section = text.closest('section');
   gsap.from(splitText.words, {
     opacity: 0,
     ease: 'none',
     stagger: 1,
-    duration: 5,
+    duration: 2, // Adjust this value to make the animation faster
     scrollTrigger: {
       trigger: section,
       start: 'top top', 
-      end: () => `+=${window.innerHeight * 5}px`,
+      end: () => `+=${window.innerHeight * 3}px`,
       scrub: true,
       // markers: true,
       pin: true,
     }
   })
+
 })
+
+
+// Reveal animation
+const setTextRevealAnimations = () => {
+  document.querySelectorAll('.text-reveal').forEach(text => {
+    // Split text
+    let splitText = new SplitType(text);
+
+    splitText.lines.forEach(line => {
+      const lineWrapper = document.createElement('div');
+      lineWrapper.classList.add('line-wrapper');
+      text.insertAdjacentElement('beforeEnd', lineWrapper);
+      lineWrapper.appendChild(line);
+    })
+
+    // Animation
+    const section = text.closest('section');
+    gsap.to(splitText.lines, {
+      y: 0,
+      ease: "power1.inOut",
+      stagger: .15,
+      scrollTrigger: {
+        trigger: text,
+        start: 'top bottom', 
+        // markers: true,
+        toggleActions: 'play reset play reset'
+      }
+    })
+  });
+}
+
+setTextRevealAnimations();
+
+// Handle resize
+const resizeObserver = new ResizeObserver(
+  debounce(([entry]) => {
+    setTextRevealAnimations();
+  }, 500)
+)
+resizeObserver.observe(document.body)
