5eef2efcbd8c79e9f14dd8693198ab092320cd25
Basemism <95645899+Basemism@users.noreply.github.com>
2024-03-07 12:24:18 +0000

    z

--- script.js
+++ script.js
@@ -1,58 +1,24 @@
-const debounce = (func, timeout = 300) => {
-    let timer;
-    return (...args) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => { func.apply(this, args); }, timeout);
-    };
-  }
-  
-  gsap.registerPlugin(ScrollTrigger);
-  
-  // Scroll
-  const lenis = new Lenis({
-    lerp: 0.07
-  });
-  
-  lenis.on('scroll', ScrollTrigger.update)
-  gsap.ticker.add((time)=>{
-    lenis.raf(time * 1000)
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis({
+  lerp: 0.07
+});
+
+lenis.on('scroll', ScrollTrigger.update)
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+const sections = document.querySelectorAll('.section .inner');
+sections?.forEach(section => {
+  gsap.to(section, {
+    yPercent: 100,
+    ease: 'none',
+    scrollTrigger: {
+      trigger: section,
+      start: 'bottom bottom',
+      end: 'bottom top',
+      scrub: true
+    }
   })
-  
-  // Reveal animation
-  const setTextRevealAnimations = () => {
-    document.querySelectorAll('.text-reveal').forEach(text => {
-      // Split text
-      let splitText = new SplitType(text);
-  
-      splitText.lines.forEach(line => {
-        const lineWrapper = document.createElement('div');
-        lineWrapper.classList.add('line-wrapper');
-        text.insertAdjacentElement('beforeEnd', lineWrapper);
-        lineWrapper.appendChild(line);
-      })
-  
-      // Animation
-      const section = text.closest('section');
-      gsap.to(splitText.lines, {
-        y: 0,
-        ease: "power1.inOut",
-        stagger: .15,
-        scrollTrigger: {
-          trigger: text,
-          start: 'top bottom', 
-          // markers: true,
-          toggleActions: 'play reset play reset'
-        }
-      })
-    });
-  }
-  
-  setTextRevealAnimations();
-  
-  // Handle resize
-  const resizeObserver = new ResizeObserver(
-    debounce(([entry]) => {
-      setTextRevealAnimations();
-    }, 500)
-  )
-  resizeObserver.observe(document.body)
+})
